61037ef29de5668707eac54ee569a7fa835ba2c0
latef02 <milanjack2@gmail.com>
2024-02-16 22:12:14 +0100

    gim

---
+++ quotes.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#double quotes 
+var=2020
+echo "this is  the test script double quotes $var"
+#single quotes 
+echo 'this is single quotes: $var '
+#command quote
+echo "this is command quotes: `pwd` #alt+7 "
+#end
+
