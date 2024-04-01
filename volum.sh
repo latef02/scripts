e9ee993a4c597bfb19cc41d2a1b902b801db65b7
latef02 <milanjack2@gmail.com>
2024-04-01 00:01:31 +0000

    volume script

--- volum.sh
+++ volum.sh
@@ -2,5 +2,28 @@
 arg=$1
 [ $arg = "Up"   ] && pactl set-sink-volume 0 +10%
 [ $arg = "Down" ] && pactl set-sink-volume 0 -10%
+
+get_vol=$(vol -get); \
+dunstify "Volume: "$get_vol -t 1000 -r 1
+
+
+
+#VOLUME_INFO=$(pactl get-sink-volume 0 | grep -Eo '[0-9]+%')
+#if [[ $1 == "-inc" ]]; then
+#  notify-send "Volume Up  ($VOLUME_INFO)"
+#else
+#  notify-send "Volume Down  ($VOLUME_INFO)"
+#fi
+
 pkill -RTMIN+30 dwmblocks
+
+
+
+
+
+#{super + Up, super + Down}
+#{pactl set-sink-volume 0 +10%,pactl set-so,c-volume 0 -10%}; \
+#get_vol=$(vol -get | cut -f 1 -d '.'); \
+#dunstify "  Volume: "$get_vol -t 1000 -r 1
+#pkill -RTMIN+30 dwmblocks
 
